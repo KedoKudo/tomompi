c13d3cf98fd88f699862565c7a1496d557d1ea74
Chen <zdms1987@hotmail.com>
2018-10-15 16:23:59 -0500

    nexusexception class can be a header only class

--- src/nexusexceptionclass.h
+++ src/nexusexceptionclass.h
@@ -3,20 +3,36 @@
 #define NexusExceptionClassH
 //---------------------------------------------------------------------------
 
+#include "string.h"
+
 class NexusExceptionClass
 {
 public:
-    NexusExceptionClass (NexusExceptionClass &old_class);
+    NexusExceptionClass (NexusExceptionClass &old_class){
+        strcpy (error_string, old_class.getErrorString());
+	    strcpy (error_type, old_class.getErrorType());
+    };
 
-	NexusExceptionClass (char *error_string);
-	NexusExceptionClass (char *error_string, char *error_type);
+	NexusExceptionClass (char *error){
+        strcpy (error_string, error);
+    };
 
-    char *getErrorString (void);
-    char *getErrorType (void);
+	NexusExceptionClass (char *error, char *type){
+        strcpy (error_string, error);
+	    strcpy (error_type, type);
+    };
+
+    char *getErrorString (void){
+        return (error_string);
+    };
+
+    char *getErrorType (void){
+        return (error_type);
+    };
 
 private:
-char	error_string[256],
-		error_type[256];
+    char	error_string[256],
+    		error_type[256];
 };
 
 #endif
