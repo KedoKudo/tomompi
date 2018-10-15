1d4199f2e892ff86b6bc78e581d63d1748e82fcf
Chen <zdms1987@hotmail.com>
2018-10-15 11:32:58 -0500

    code cosmetic update

--- src/recon_algorithm.cpp
+++ src/recon_algorithm.cpp
@@ -12,36 +12,31 @@
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-Filter::Filter()
-{
-  filter_type = FILTER_NONE;
-}
+// Filter::Filter()
+// {
+//   filter_type = FILTER_NONE;
+// }
 
 //---------------------------------------------------------------------------
-
-void Filter::setFilter (int filter)
-{
+void Filter::setFilter (int filter){
   filter_type = filter;
 }
 
 //---------------------------------------------------------------------------
-
-int Filter::getFilter (void)
-{
+int Filter::getFilter (void){
   return (filter_type);
 }
 
 //---------------------------------------------------------------------------
-
 float Filter::filterData (float x)
 {
   switch (filter_type) {
-  case FILTER_NONE : break;
-  case FILTER_SHEPP_LOGAN : x = shlo (x); break;
-  case FILTER_HANN : x = hann (x); break;
-  case FILTER_HAMMING : x = hamm (x); break;
-  case FILTER_RAMP : x = ramp (x); break;
-  default : break;
+    case FILTER_NONE : break;
+    case FILTER_SHEPP_LOGAN : x = shlo (x); break;
+    case FILTER_HANN : x = hann (x); break;
+    case FILTER_HAMMING : x = hamm (x); break;
+    case FILTER_RAMP : x = ramp (x); break;
+    default : break;
   }
 
   return (x);
@@ -49,26 +44,27 @@ float Filter::filterData (float x)
 
 //---------------------------------------------------------------------------
 
-float Filter::shlo (float x)
-{	/* Shepp-Logan filter */
+float Filter::shlo (float x){	
+  /* Shepp-Logan filter */
   return fabs(sin(PI*x)/PI);
 }
 
 //---------------------------------------------------------------------------
-float Filter::hann (float x)
-{	/* Hann filter */
+// This is supposed to be the best filter for Beamline 1 data
+float Filter::hann (float x){	
+  /* Hann filter */
   return fabs(x)*0.5*(1.0+cos(2*PI*x));
 }
 
 //---------------------------------------------------------------------------
-float Filter::hamm (float x)
-{	/* Hamming filter */
+float Filter::hamm (float x){	
+  /* Hamming filter */
   return fabs(x)*(0.54+0.46*cos(2*PI*x));
 }
 
 //---------------------------------------------------------------------------
-float Filter::ramp (float x)
-{	/* Ramp filter */
+float Filter::ramp (float x){	
+  /* Ramp filter */
   return fabs(x);
 }
 
@@ -78,21 +74,19 @@ float Filter::ramp (float x)
 //---------------------------------------------------------------------------
 //---------------------------------------------------------------------------
 
-ReconAlgorithm::ReconAlgorithm (void)
-{
-  theta_list = NULL;
+// ReconAlgorithm::ReconAlgorithm (void){
+//   theta_list = NULL;
 
-  sinogram1 = NULL;
-  sinogram2 = NULL;
+//   sinogram1 = NULL;
+//   sinogram2 = NULL;
 
-  reconstruction1 = NULL;
-  reconstruction2 = NULL;
-}
+//   reconstruction1 = NULL;
+//   reconstruction2 = NULL;
+// }
 
 //---------------------------------------------------------------------------
 
-void ReconAlgorithm::acknowledgements (LogFileClass *acknowledge_file)
-{
+void ReconAlgorithm::acknowledgements (LogFileClass *acknowledge_file){
   acknowledge_file->Message ("__________________________________________________________________");
   acknowledge_file->Message ("ReconAlgorithm class");
   acknowledge_file->Message ("");
@@ -116,85 +110,75 @@ void ReconAlgorithm::acknowledgements (LogFileClass *acknowledge_file)
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::numberOfSinogramsNeeded (void)
-{
+int ReconAlgorithm::numberOfSinogramsNeeded (void){
   return (num_sinograms_needed);
 }
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::numberOfReconstructions (void)
-{
+int ReconAlgorithm::numberOfReconstructions (void){
   return (num_sinograms_needed);
 }
 
 //---------------------------------------------------------------------------
 
-void ReconAlgorithm::setThetaList (float *theta_list, int theta_list_size)
-{
+void ReconAlgorithm::setThetaList (float *theta_list, int theta_list_size){
   this->theta_list = theta_list;
   this->theta_list_size = theta_list_size;
 }
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::getThetaListSize (void)
-{
+int ReconAlgorithm::getThetaListSize (void){
   return (theta_list_size);
 }
 
 //---------------------------------------------------------------------------
 
-float *ReconAlgorithm::getThetaList (void)
-{
+float *ReconAlgorithm::getThetaList (void){
   return (theta_list);
 }
 
 //---------------------------------------------------------------------------
 
-void ReconAlgorithm::setSinogramDimensions (int x_dim, int y_dim)
-{
+void ReconAlgorithm::setSinogramDimensions (int x_dim, int y_dim){
   sinogram_x_dim = x_dim;
   sinogram_y_dim = y_dim;
 }
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::getSinogramXDimension (void)
-{
+int ReconAlgorithm::getSinogramXDimension (void){
   return (sinogram_x_dim);
 }
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::getSinogramYDimension (void)
-{
+int ReconAlgorithm::getSinogramYDimension (void){
   return (sinogram_y_dim);
 }
 
 //---------------------------------------------------------------------------
 
-void ReconAlgorithm::setFilter (int filter_name)
-{
+void ReconAlgorithm::setFilter (int filter_name){
   filter.setFilter(filter_name);
 }
 
 //---------------------------------------------------------------------------
 
-int ReconAlgorithm::getFilter (void)
-{
+int ReconAlgorithm::getFilter (void){
   return (filter.getFilter());
 }
 
 //---------------------------------------------------------------------------
 
-void ReconAlgorithm::setSinoAndReconBuffers (int number, float *sinogram_address, float *reconstruction_address)
-{
-  if (number == 1)
-    {
-      sinogram1 = sinogram_address;
-      reconstruction1 = reconstruction_address;
-    }
+void ReconAlgorithm::setSinoAndReconBuffers (int    number, 
+                                             float *sinogram_address, 
+                                             float *reconstruction_address){
+  if (number == 1){
+    sinogram1 = sinogram_address;
+    reconstruction1 = reconstruction_address;
+  }
 }
 
 //---------------------------------------------------------------------------
--- src/recon_algorithm.h
+++ src/recon_algorithm.h
@@ -8,12 +8,13 @@
 
 #include "logfileclass.h"
 
-extern "C" {
+extern "C" {
 #include "fft.h"
 }
 
 //_____________________________________________________________________________________
-//Defines for reconstruction filter algorithm
+
+//Defines for reconstruction filter algorithm
 
 #define         FILTER_NONE		                0
 #define         FILTER_SHEPP_LOGAN              1
@@ -34,7 +35,7 @@ typedef struct {
 class Filter
 {
 public:
-	Filter ();
+	Filter ():filter_type(FILTER_NONE){};
 
 	void setFilter (int filter);
 	int getFilter (void);
@@ -42,7 +43,7 @@ public:
 	float filterData (float x);
 
 private:
-int     filter_type;
+	int     filter_type;
 
 	float shlo (float x);	/* Shepp-Logan filter */
 	float hann (float x);	/* Hann filter */
@@ -52,24 +53,28 @@ int     filter_type;
 
 //---------------------------------------------------------------------------
 
-class ReconAlgorithm
-{
+class ReconAlgorithm{
 public:
-	ReconAlgorithm (void);
+	ReconAlgorithm (void):
+		theta_list(NULL),
+		sinogram1(NULL),
+		sinogram2(NULL),
+		reconstruction1(NULL),
+		reconstruction2(NULL){};
 
 	int numberOfSinogramsNeeded (void);
 	int numberOfReconstructions (void);
 
-	void setThetaList (float *theta_list, int theta_list_size);
-	int getThetaListSize (void);
+	void   setThetaList (float *theta_list, int theta_list_size);
+	int    getThetaListSize (void);
 	float *getThetaList (void);
 
 	void setSinogramDimensions (int x_dim, int y_dim);
-	int getSinogramXDimension (void);
-	int getSinogramYDimension (void);
+	int  getSinogramXDimension (void);
+	int  getSinogramYDimension (void);
 
 	void setFilter (int filter_name);
-	int getFilter (void);
+	int  getFilter (void);
 
 	virtual void setSinoAndReconBuffers (int number, float *sinogram_address, float *reconstruction_address);
 
@@ -80,16 +85,16 @@ public:
 	static void acknowledgements (LogFileClass *acknowledge_file);
 
 protected:
-int             num_sinograms_needed,
-				theta_list_size;
-unsigned long   sinogram_x_dim,
-				sinogram_y_dim;
-float           *theta_list,
-				*sinogram1,
-				*sinogram2,
-				*reconstruction1,
-				*reconstruction2;
-Filter          filter;
+	int             num_sinograms_needed,
+					theta_list_size;
+	unsigned long   sinogram_x_dim,
+					sinogram_y_dim;
+	float           *theta_list,
+					*sinogram1,
+					*sinogram2,
+					*reconstruction1,
+					*reconstruction2;
+	Filter          filter;
 
 };
 //---------------------------------------------------------------------------
